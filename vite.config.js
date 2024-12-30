da6365c75d23ee556f34e112e050087c7acc454a
Vishnu Mandlesara <140898898+Vishnu-0609@users.noreply.github.com>
2024-12-30 18:50:13 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
-})
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: './_redirects', // Path to your _redirects file
+          dest: '.'            // Place it in the root of dist
+        }
+      ]
+    })
+  ],
+});
